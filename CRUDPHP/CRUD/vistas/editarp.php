3e8b1725a9b1690110960f60a9e846dd77d46e9c
Yulissa <yestradac@unemi.edu.ec>
2025-08-30 12:04:21 -0500

    Mejora: Corregido error en la funcionalidad de editar

--- CRUDPHP/CRUD/vistas/editarp.php
+++ CRUDPHP/CRUD/vistas/editarp.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>edición de datos</title>
+	<title>edición de los datos</title>
 	 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 	 <?php 
       include'../config/conexion.php';
@@ -21,7 +21,7 @@
     <div class="col-xl-6 col-lg-6 col-md-8 col-12">
     <div class="card">
     <div class="card-header bg-warning text-white">
-     <span><strong>editar datos</strong></span>   
+     <span><strong>editar los datos</strong></span>   
     </div>  
     <form action="../logica/editar.php" method="GET">
     	<input class="d-none" type="text" name="txId" value="<?=$Id?>">
@@ -35,18 +35,18 @@
     <input type="text" name="txtdni" class="form-control" placeholder="digite su dni.." value="<?=$person[0]?>">
     </div>  
     <div class="form-group">
-    <label>APELLIDOS</label>
+    <label>APELLIDOS COMPLETOS</label>
     <input type="text" name="txtapellidos" class="form-control" placeholder="dingrese sus apellidos completos.." value="<?=$person[2]?>" >
     </div>  
     <div class="form-group">
-    <label>NOMBRES</label>
+    <label>NOMBRES COMPLETOS</label>
     <input type="text" name="txtnombres" class="form-control" placeholder="ingrese sus nombres completos..." value="<?=$person[1]?>">
     </div>  
 <?php } ?>
     </div>  
     </div>
     <div class="card-footer">
-     <button class="btn btn-outline-success">Guardar Cambios</button> 
+     <button class="btn btn-outline-success">Guardar Cambios Realizados</button> 
     </div>
     </form>
     </div>  
